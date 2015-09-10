85a96116e4534d70a1e09ffd63073d17860f024f
Joao Moreno <jomo@microsoft.com>
2015-09-10 16:21:41 +0200

    compile to bin

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 var gulp = require('gulp');
 var tsb = require('gulp-tsb');
+var filter = require('gulp-filter');
+var rimraf = require('rimraf');
 var tsconfig = require('./tsconfig.json')
-
 var compilation = tsb.create(tsconfig.compilerOptions);
 
+gulp.task('clean', function (cb) {
+	rimraf('bin', cb);
+})
+
 var sources = [
-	'src/**/*.ts',
-	'test/**/*.ts',
+	'src/**',
+	'test/**',
 	'typings/**/*.ts'
 ];
 
-gulp.task('compile', function () {
-	return gulp.src(sources)
+gulp.task('compile', ['clean'], function () {
+	var typescriptsFilter = filter('**/*.ts', { restore: true });
+	
+	return gulp.src(sources, { base: '.' })
+		.pipe(typescriptsFilter)
 		.pipe(compilation())
+		.pipe(typescriptsFilter.restore)
 		.pipe(gulp.dest('bin'));
 });
